98328c01c32f24543486b71b36e33358a88d051a
Daniel Maxim Babiak <daniel@wave.com>
2020-10-22 17:33:52 -0400

    more stuff

--- .vimrc
+++ .vimrc
@@ -35,3 +35,26 @@ set timeoutlen=1000 ttimeoutlen=0
 let g:loaded_matchparen=1
 
 set clipboard=unnamedplus
+
+vmap <C-r> yq:pIr !<CR>
+nnoremap <S-r> ma<S-v>yq:pIr !<CR>`a
+
+set background=dark
+
+set rtp+=~/.fzf
+
+runtime ftplugin/man.vim
+set keywordprg=:Man
+
+
+nnoremap <S-t> :!vman <C-R>=expand("<cword>")<CR><CR><CR>
+
+call plug#begin('~/.vim/plugged')
+Plug 'dracula/vim', { 'as': 'dracula' }
+call plug#end()
+
+syntax enable
+colorscheme dracula
+
+command GY execute 'silent !/usr/bin/git' 'add' shellescape(expand('%:p')) | execute ':qa'
+command GN execute 'silent !/usr/bin/git' 'checkout' shellescape(expand('%:p')) | execute ':qa'
--- funcs.sh
+++ funcs.sh
@@ -26,3 +26,36 @@ resync-prod() {
   && git merge --ff-only "$1"  \
   && git push origin prod:prod
 }
+
+path() {
+  echo -n "$(pwd)/${1}" | xclip -selection clipboard
+}
+
+
+
+# list every directory on $PATH
+all-executables() {
+  ls $(echo $PATH | awk 'BEGIN { RS = ":" } {print $0}' | sort | uniq | egrep -v '^$') | sort | uniq
+}
+
+quip() {
+  (firefox https://wavemm.quip.com/bSTAOAxRxZQ &> /dev/null &)
+}
+
+slack() {
+  (firefox https://app.slack.com/client/T5XKZ9F8X/CN8UZ316W/thread/C5WUUKNKU-1575667988.299100 &> /dev/null &)
+}
+
+# If I background the subshell, how can it prompt for sudo password?
+snail() {
+  sudo bash <<"EOF"
+(
+  xhost +SI:localuser:root \
+  && sudo xkeysnail -q /home/dmb/xkeysnail_config.py
+) &> /dev/null &
+EOF
+}
+
+up() {
+  cp -f "$1" "/home/dmb/uploads/${1}"
+}
